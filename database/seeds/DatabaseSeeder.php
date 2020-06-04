b46fda91322ed0687fdae05ac38de9185c9f0012
RimonShil <Rimonroy.cse@gmail.com>
2020-06-04 13:40:27 +0600

    Factory&Seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Model\Category;
+use App\Model\Like;
+use App\Model\Question;
+use App\Model\Reply;
+use App\User;
 use Illuminate\Database\Seeder;
+use Psy\CodeCleaner\FunctionReturnInWriteContextPass;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +17,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+         factory(User::class, 10)->create();
+         factory(Category::class, 5)->create();
+         factory(Question::class, 5)->create();
+         factory(Reply::class, 50)->create();
+         factory(Like::class, 50)->create();
     }
 }
