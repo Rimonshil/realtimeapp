32250d5edc0952c12a61d653b78236c25bf81e52
RimonShil <Rimonroy.cse@gmail.com>
2020-06-05 23:25:00 +0600

    vue-router vue-component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
+
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+window.Vue=require('vue');
+
 require('./bootstrap');
+
+Vue.use(Vuetify)
+
+import Home from './components/Home.vue';
+import router from './Router/Router.js';
+
+new Vue({
+    vuetify : new Vuetify(),
+    el:'#app',
+    router,
+    components:{
+        AppHome: Home
+    }
+
+});
+  
---
+++ resources/js/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <v-footer>
+        RealTimeApp
+    <v-spacer></v-spacer>
+    <div>&copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
